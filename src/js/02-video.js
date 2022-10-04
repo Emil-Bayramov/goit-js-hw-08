ed491b515eea26bb49154c8222146232893e7689
Emil-Bayramov <105307406+Emil-Bayramov@users.noreply.github.com>
2022-10-04 12:05:36 +0300

    20221004/1205

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+'use strict';
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+player.on('timeupdate', throttle(onTimeUpdate, 888));
+
+
+function onTimeUpdate (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds)
+}
+
+const timeUpd = Number(localStorage.getItem('videoplayer-current-time'))
+
+player.setCurrentTime(timeUpd).then(function(seconds) {
+   
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+'use strict';
+
+import throttle from 'lodash.throttle'
+
+const form = document.querySelector('form')
+const inputEmail = document.querySelector('input')
+const inputMessage = document.querySelector('textarea')
+
+const formData = {}
+
+
+
+checkLocalStorage()
+function checkLocalStorage() {
+    const localStorageData = localStorage.getItem('feedback-form-state') 
+    const parsedlocalStorageData = JSON.parse(localStorageData) 
+    if (parsedlocalStorageData) {
+        inputEmail.value = parsedlocalStorageData[inputEmail.name] || ''
+        inputMessage.value = parsedlocalStorageData[inputMessage.name] || ''
+    } 
+}
+
+
+
+form.addEventListener('input', throttle(onFormInput, 444) );
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value 
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData)) 
+
+}
+
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault()
+    if (inputEmail.value === "" || inputMessage.value === "") {
+        return alert("Please, fill in all the fields!")
+    }
+    const submitedData = JSON.parse(localStorage.getItem('feedback-form-state'))
+    console.log(submitedData)
+    localStorage.removeItem('feedback-form-state');
+    form.reset()
+}
+
+
+
 
